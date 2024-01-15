605343ac7b5fce4bf83c37c9b681f629ab04b119
hiwatashi-michiya <115054875+hiwatashi-michiya@users.noreply.github.com>
2024-01-15 10:46:01 +0900

    06_02_確認課題

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
-﻿#include <stdio.h>
+﻿#include <thread>
+#include <iostream>
+
+void ShowThread(int num) {
+
+	std::cout << "thread " << num << std::endl;
+
+}
 
 int main() {
 
+	std::thread thread1(ShowThread, 1);
+	thread1.join();
+	std::thread thread2(ShowThread, 2);
+	thread2.join();
 
-	
-	
+	std::thread thread3(ShowThread, 3);
+	thread3.join();
 
 	return 0;
 }
